2aaab8fb0f355c4de21ccbac5d42780d73735c52
Diego Perez <dapohp@gmail.com>
2019-06-13 11:44:06 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'internazionalization';
+
+	titles = [
+		{ code: 'en', label: 'Welcome to Internazionalization!!!' },
+		{ code: 'es', label: 'Bienvenido a Internacionalizacion!!!' }
+	];
+
+	languages = [
+		{ code: 'en', label: 'English' },
+		{ code: 'es', label: 'Espanol'}
+	];
+
+	constructor(@Inject(LOCALE_ID) protected localeId: string) {
+		console.log(this.titles[0].code);
+		console.log(localeId.substr(0, 2));
+
+		for (let title of this.titles) {
+			if (localeId.substr(0, 2) === title.code) {
+				console.log('Este es pillelo');
+			}
+		}
+
+
+
+	}
+
 }
